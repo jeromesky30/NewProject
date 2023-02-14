737f1407eb3e9860440d4c86601d01f4ccf6f407
jeromesky30 <118332472+jeromesky30@users.noreply.github.com>
2023-02-14 17:27:42 +0800

    Add files via upload

---
+++ about.blade.php
@@ -0,0 +1,141 @@
+@extends('frontend.layouts.master')
+@section('content')
+<div class="row tm-row tm-mb-45">
+    <div class="col-12">
+        <hr class="tm-hr-primary tm-mb-55">
+        <img src="{{ asset('frontend_assets') }}/img/about-4.jpg" alt="Image" class="img-fluid" style="width: 700px; height: 500px;>
+    </div>
+</div>
+<div class="row tm-row tm-mb-40">
+    <div class="col-12">
+        <div class="mb-4">
+            <h2 class="pt-2 tm-mb-40 tm-color-primary tm-post-title">About Foodhub Blog</h2>
+            <p>
+                Food is what you eat every day for survival. Enjoying food is more fun when you share this with everyone. If you are ready and equipped, it’s never too late to explore and try this new trend of food blogging. So, why not give it a try and let’s see how many subscribers you can get? Who knows, I will be one of your avid followers in the future? Keep posting!
+            </p>
+            <p>
+                Food blogging is the byproduct of a love for food and wanting to share that with the world by means of traveling and social media platforms. Food blogging is a blanket term that covers all kinds of different content. Some of the most common and popular types of food blogs include:
+            </p>
+        </div>
+    </div>
+</div>
+<div class="row tm-row tm-mb-120">
+    <div class="col-lg-4 tm-about-col">
+        <div class="tm-bg-gray tm-about-pad">
+            <div class="text-center tm-mt-40 tm-mb-60">
+                <i class="fas fa-bezier-curve fa-4x tm-color-primary"></i>
+            </div>
+            <h2 class="mb-3 tm-color-primary tm-post-title">Background</h2>
+            <p class="mb-0 tm-line-height-short">
+                Foodhub Blog was created by 4 Fullstack Web Developer,this web application was appoved by the Instructor of boothcamp Kodego Community.
+            </p>
+        </div>
+    </div>
+    <div class="col-lg-4 tm-about-col">
+        <div class="tm-bg-gray tm-about-pad">
+            <div class="text-center tm-mt-40 tm-mb-60">
+                <i class="fas fa-users-cog fa-4x tm-color-primary"></i>
+            </div>
+            <h2 class="mb-3 tm-color-primary tm-post-title">Teamwork</h2>
+            <p class="mb-0 tm-line-height-short">
+                This project was made by group 11 capstone project, with the team of 4 members,including Jerome Pingol,Ma.Ivy Vanessa,Charlene Lopez and Benedict Pableo
+            </p>
+        </div>
+    </div>
+    <div class="col-lg-4 tm-about-col">
+        <div class="tm-bg-gray tm-about-pad">
+            <div class="text-center tm-mt-40 tm-mb-60">
+                <i class="fab fa-creative-commons-sampling fa-4x tm-color-primary"></i>
+            </div>
+            <h2 class="mb-3 tm-color-primary tm-post-title">Our Core Value</h2>
+            <p class="mb-0 tm-line-height-short">
+                Authentic – be real, open and informal. Committed – have the courage and standard for a better company, a better world, a better you. Trusted – do the right thing. Build genuine relationships with our customers, stakeholders and each other.
+            </p>
+        </div>
+    </div>
+</div>
+<div class="row tm-row tm-mb-60">
+    <div class="col-12">
+        <hr class="tm-hr-primary  tm-mb-55">
+    </div>
+    <div class="col-lg-6 tm-mb-60 tm-person-col">
+        <div class="media tm-person">
+            <img src="{{ asset('frontend_assets') }}/img/jerome.jpg" alt="Image" class="img-fluid mr-4"  style="width: 200px; height: 200px;">
+            <div class="media-body">
+                <h2 class="tm-color-primary tm-post-title mb-2">Jerome Pingol</h2>
+                <h3 class="tm-h3 mb-3">Fullstack Web Developer</h3>
+                <a rel="nofollow" href="https://www.facebook.com/OnlineRakitero26" class="tm-social-link">
+                    <i class="fab fa-facebook tm-social-icon"></i>
+                </a>
+                <a href="https://instagram.com" class="tm-social-link">
+                    <i class="fab fa-instagram tm-social-icon"></i>
+                </a>
+                
+                
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6 tm-mb-60 tm-person-col">
+        <div class="media tm-person">
+            <img src="{{ asset('frontend_assets') }}/img/ivan.jpg" alt="Image" class="img-fluid mr-4"  style="width: 200px; height: 200px;">
+            <div class="media-body">
+                <h2 class="tm-color-primary tm-post-title mb-2">Ma. Ivy Vanessa</h2>
+                <h3 class="tm-h3 mb-3">Fullstack Web Developer</h3>
+                <a rel="nofollow" href="https://www.facebook.com/OnlineRakitero26" class="tm-social-link">
+                    <i class="fab fa-facebook tm-social-icon"></i>
+                </a>
+                <a href="https://instagram.com" class="tm-social-link">
+                    <i class="fab fa-instagram tm-social-icon"></i>
+                </a>
+                
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6 tm-mb-60 tm-person-col">
+        <div class="media tm-person">
+            <img src="{{ asset('frontend_assets') }}/img/about-04.jpg" alt="Image" class="img-fluid mr-4"  style="width: 200px; height: 200px;">
+            <div class="media-body">
+                <h2 class="tm-color-primary tm-post-title mb-2">Benedick Pableo</h2>
+                <h3 class="tm-h3 mb-3">Fullstack Web Developer</h3>
+                <a rel="nofollow" href="https://www.facebook.com/OnlineRakitero26" class="tm-social-link">
+                    <i class="fab fa-facebook tm-social-icon"></i>
+                </a>
+                <a href="https://instagram.com" class="tm-social-link">
+                    <i class="fab fa-instagram tm-social-icon"></i>
+                </a>
+               
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6 tm-mb-60 tm-person-col">
+        <div class="media tm-person">
+            <img src="{{ asset('frontend_assets') }}/img/charlene.jpg" alt="Image" class="img-fluid mr-4"  style="width: 200px; height: 200px;">
+            <div class="media-body">
+                <h2 class="tm-color-primary tm-post-title mb-2">Charlene Lopez</h2>
+                <h3 class="tm-h3 mb-3">Fullstack Web Developer</h3>
+                <a rel="nofollow" href="https://www.facebook.com/OnlineRakitero26" class="tm-social-link">
+                    <i class="fab fa-facebook tm-social-icon"></i>
+                </a>
+                <a href="https://instagram.com" class="tm-social-link">
+                    <i class="fab fa-instagram tm-social-icon"></i>
+                </a>
+                
+            </div>
+        </div>
+    </div>
+    <div id="editor">This is some sample content.</div>
+    <script>
+            ClassicEditor
+                    .create( document.querySelector( '#editor' ) )
+                    .then( editor => {
+                            console.log( editor );
+                    } )
+                    .catch( error => {
+                            console.error( error );
+                    } );
+    </script>
+</div>
+
+<script src="https://cdn.ckeditor.com/ckeditor5/36.0.1/classic/ckeditor.js"></script>
+
+@endsection
